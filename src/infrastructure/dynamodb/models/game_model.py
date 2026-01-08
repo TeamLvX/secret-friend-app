c75a8cb5111a9dea823572eda33f7455f63e42ba
drixev <dev.andy.attitude803@passinbox.com>
2026-01-07 19:43:18 -0500

    Completing SFP-6

--- src/infrastructure/dynamodb/models/game_model.py
+++ src/infrastructure/dynamodb/models/game_model.py
@@ -1,9 +1,12 @@
 from pynamodb.models import Model
 from pynamodb.attributes import (UnicodeAttribute, BooleanAttribute, NumberAttribute, UTCDateTimeAttribute)
+from src.core.config import settings
 
 class GameModel(Model):
     class Meta:
         table_name = "game_collection"
+        host = settings.dynamodb_host
+        region = settings.aws_region
 
 '''
     name:
