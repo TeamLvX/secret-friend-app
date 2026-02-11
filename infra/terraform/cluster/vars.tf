8eef1aa96d2a3a7c49764bd7767fe5e25ba06a1f
Christian Rivera <crivera2093@gmail.com>
2026-02-11 12:54:31 -0500

    feat: add terraform scripts for ecs cluster provisioning and pipeline for cluster deployment. (#37)

--- infra/terraform/cluster/vars.tf
+++ infra/terraform/cluster/vars.tf
@@ -37,21 +37,3 @@ variable "DATA_CLASSIFICATION" {
   type        = string
   description = "A classification for the type of data managed by resources"
 }
-
-variable "ASSIGNMENTS_TABLE_NAME" {
-  default     = "assignment_collection"
-  type        = string
-  description = "dynamodb table to support assignment functionality"
-}
-
-variable "GROUPS_TABLE_NAME" {
-  default     = "group_collection"
-  type        = string
-  description = "dynamodb table to support group-game functionality"
-}
-
-variable "PARTICIPANTS_TABLE_NAME" {
-  default     = "participant_collection"
-  type        = string
-  description = "dynamodb table to support participant functionality"
-}
