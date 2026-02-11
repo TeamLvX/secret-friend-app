a35f6656c0785425af6a73e563b048899a026849
Christian Rivera <crivera2093@gmail.com>
2026-02-11 12:49:54 -0500

    feat: add terraform scripts for ecs cluster provisioning and pipeline for cluster deployment.

---
+++ infra/terraform/cluster/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.28.0"
+    }
+  }
+
+  # Backend configuration for remote state storage
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = var.AWS_REGION
+}
